7b882fae3c9b875bfcccdf159a7fb6659e002aed
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-13 10:47:32 -0700

    Add files via upload

---
+++ Ransom Note.py
@@ -0,0 +1,18 @@
+# 383. Ransom Note
+
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ran = sorted(list(ransomNote))
+        mag = sorted(list(magazine))
+        
+        for char in mag:
+            if ran and char == ran[0]:
+                ran.pop(0)
+        if ran:
+            return False
+        else:
+            return True
